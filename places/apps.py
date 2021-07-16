b8cc48b10736acd69a1407c1b1632b9774980d4e
ChyngyzTorogeldiev <www.araket@gmail.com>
2021-07-16 16:20:10 +0600

    created project with 22 apps

--- places/apps.py
+++ places/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PlycesConfig(AppConfig):
+class PlacesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'plyces'
+    name = 'places'
